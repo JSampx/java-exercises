4bc16a3bfd144220f0bc420b354b93d9efc43505
João Sampaio <joao.sampaio.andrade@gmail.com>
2025-01-19 11:40:13 -0300

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import entities.Circle;
+
 public class Main {
     public static void main(String[] args) {
+        Circle circle = new Circle(4);
+        System.out.printf("%.2f" + "m2", circle.getArea());
 
-        System.out.println("Hello, World!");
     }
 }
