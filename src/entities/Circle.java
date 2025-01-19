47e5cd6f0bb6900659bd80aca6b16cc6fb80f3e2
João Sampaio <joao.sampaio.andrade@gmail.com>
2025-01-19 10:58:14 -0300

    Created area interface

--- src/entities/Circle.java
+++ src/entities/Circle.java
@@ -1,14 +1,15 @@
 package entities;
 
-public class Circle extends Shape{
+public class Circle extends Shape implements area{
     private double radius;
 
     public Circle(double radius) {
         this.radius = radius;
     }
 
+
     @Override
-    public double area(){
+    public double getArea() {
         return Math.PI * radius * radius;
     }
 }
